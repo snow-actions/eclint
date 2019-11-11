32b3a2a43d24a69a3ea0063b6884e29bfd4d8277
SnowCait <SnowCait@users.noreply.github.com>
2019-11-11 22:08:31 +0900

    Fix

--- eclint.js
+++ eclint.js
@@ -1,7 +1,7 @@
 const exec = require('@actions/exec');
 
 let eclint = async function (args) {
-  await exec.exec('npm run eclint ' + args);
+  await exec.exec('npm run eclint -- ' + args);
 }
 
 module.exports = eclint;
